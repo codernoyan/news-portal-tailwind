21b01aa99957f09ff9677c1ea718e45612a116be
codernoyan <coder.noyan@gmail.com>
2022-09-03 02:30:14 +0600

    modal scroll fixed

--- js/details.js
+++ js/details.js
@@ -11,6 +11,16 @@ const showNewsDetails = async (newsId) => {
 
 const displayNewsDetails = news => {
     console.log(news);
+
+    const { details, img: authorImage, image_url, name: authorName} = news;
+
     const modalTitle = document.getElementById('modal-title');
     modalTitle.innerText = news.title;
+
+    // display data on modal
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = `
+        <img src=${image_url}>    
+        <p>${details}</p>
+    `;
 }
