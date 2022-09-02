725922914bcba52393602cc5b195a3f44dc84b76
codernoyan <coder.noyan@gmail.com>
2022-09-03 03:14:27 +0600

    validation added on author name and views

--- js/details.js
+++ js/details.js
@@ -27,7 +27,7 @@ const displayNewsDetails = news => {
             <div class="flex items-center gap-2">
                 <img src=${authorImage} class="w-11 h-auto rounded-full" alt="">
                 <div>
-                <h4 class="font-medium text-sm">${authorName.length}</h4>
+                <h4 class="font-medium text-sm">${authorName.length === 0 ? 'No Data Available' : authorName}</h4>
                 </div>
             </div>
             <div class="flex items-center gap-2">
@@ -35,7 +35,7 @@ const displayNewsDetails = news => {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <h4 class="font-medium">${parseFloat((total_view/1000).toFixed(2))}M</h4>
+                <h4 class="font-medium">${total_view === 0 ? 'No Data Available' : parseFloat((total_view/1000).toFixed(2)) + 'M'}</h4>
             </div>
         </div>
     `;
