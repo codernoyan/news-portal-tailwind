fb44a103da9defd9663cce2504cb65af17e52462
codernoyan <coder.noyan@gmail.com>
2022-09-03 03:05:32 +0600

    modal created

--- js/details.js
+++ js/details.js
@@ -12,7 +12,8 @@ const showNewsDetails = async (newsId) => {
 const displayNewsDetails = news => {
     console.log(news);
 
-    const { details, img: authorImage, image_url, name: authorName} = news;
+    const { details, author, image_url, total_view} = news;
+    const { img: authorImage, name: authorName } = author;
 
     const modalTitle = document.getElementById('modal-title');
     modalTitle.innerText = news.title;
@@ -22,5 +23,20 @@ const displayNewsDetails = news => {
     modalBody.innerHTML = `
         <img src=${image_url}>    
         <p>${details}</p>
+        <div class="flex justify-around items-center gap-2 mt-2">
+            <div class="flex items-center gap-2">
+                <img src=${authorImage} class="w-11 h-auto rounded-full" alt="">
+                <div>
+                <h4 class="font-medium text-sm">${authorName.length}</h4>
+                </div>
+            </div>
+            <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <h4 class="font-medium">${parseFloat((total_view/1000).toFixed(2))}M</h4>
+            </div>
+        </div>
     `;
 }
